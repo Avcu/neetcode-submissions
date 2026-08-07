6de49754d316966f7adc95b4a4ca88be982ca8d6
Mustafa Avcu <itsavcu@gmail.com>
2026-08-07 16:40:45 -0400

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        mult = 0
+        for num in nums:
+            mult = mult ^ num
+
+        allMult = 0
+        for num in range(len(nums)+1):
+            allMult = allMult ^ num
+
+        res = 0
+        for i in range(32):
+            if (allMult & 1) != (mult & 1):
+                res = res | (1 << i) 
+            allMult = allMult >> 1
+            mult = mult >> 1
+        print(bin(res))
+        return res
