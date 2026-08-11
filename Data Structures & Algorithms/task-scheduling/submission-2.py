332dc455baa890c69abc2a2e0f78803d3d829b8a
Mustafa Avcu <itsavcu@gmail.com>
2026-08-11 13:01:54 -0400

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,42 @@
+from heapq import heappush, heappop
+from collections import deque
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        heap = []
+        q = deque()
+        dictTaskCount = {}
+
+        for task in tasks:
+            if task in dictTaskCount:
+                dictTaskCount[task] += 1
+            else:
+                dictTaskCount[task] = 1
+        
+        for task, taskCount in dictTaskCount.items():
+            heappush(heap, -taskCount)
+
+        currTime = 0
+        while heap or q:
+            # if there are new available tasks in the queue add them back to heap
+            while q and q[0][1] <= currTime:
+                poppedQ = q.popleft()
+                heappush(heap, poppedQ[0])
+
+            # run the next task in the heap
+            if heap:
+                poppedVal = heappop(heap)
+                if poppedVal != -1:
+                    q.append([poppedVal+1, currTime+n+1])
+                currTime += 1
+            else:
+                # if there is no task available wait for the next one in the queue
+                poppedQ = q.popleft()
+                currTime = poppedQ[1]
+                heappush(heap, poppedQ[0])
+
+
+        return currTime
+
+
+
