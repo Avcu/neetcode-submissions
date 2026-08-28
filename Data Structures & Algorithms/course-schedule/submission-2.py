0062d06ef4d50af8b17a17392d914b6521ddefab
Mustafa Avcu <itsavcu@gmail.com>
2026-08-28 16:09:56 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        numberOfPrereq = [0 for _ in range(numCourses)]
+        adj = [[] for _ in range(numCourses)]
+        
+        for prereqs in prerequisites:
+            course, pre = prereqs[0], prereqs[1]
+            # number of required courses
+            numberOfPrereq[course] += 1
+            # list of next courses that require this course
+            adj[pre].append(course)
+
+        q = deque()
+        seen = set()
+
+        for idx in range(len(numberOfPrereq)):
+            if numberOfPrereq[idx] == 0:
+                q.append(idx)
+                seen.add(idx)
+
+        while q:
+            currCourse = q.popleft()
+            for nextCourse in adj[currCourse]:
+                numberOfPrereq[nextCourse] -= 1
+                if numberOfPrereq[nextCourse] == 0:
+                    q.append(nextCourse)
+                    seen.add(nextCourse)
+
+        return len(seen) == numCourses
