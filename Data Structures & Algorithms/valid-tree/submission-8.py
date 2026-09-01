b2785098795a001b68a83afdbce898b6b46be236
Mustafa Avcu <itsavcu@gmail.com>
2026-09-01 12:09:57 -0400

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = [[] for _ in range(n)]
+
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+
+        seen = set()
+
+        def dfs(child, parent):
+            seen.add(child)
+
+            res = True
+            for nei in adj[child]:
+                if nei == parent:
+                    continue
+                if nei in seen:
+                    res = False
+                    break
+                res = res and dfs(nei, child)
+            return res
+
+        return dfs(0, -1) and len(seen) == n
