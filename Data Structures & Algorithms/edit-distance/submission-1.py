91121887da9be5c9e43db15ccc6bf7f26557c6f9
Mustafa Avcu <itsavcu@gmail.com>
2026-09-04 10:55:40 -0400

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+        memo = {}
+        def dfs(i, j):
+            if j == len(word2):
+                return len(word1) - i
+            if i >= len(word1):
+                return len(word2) - j
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            res = float('inf')
+            if word1[i] == word2[j]:
+                res = min(res, dfs(i+1, j+1))
+
+            # try removing (ignore the char at index i)
+            res = min(res, dfs(i+1, j) + 1)
+            # try adding
+            res = min(res, dfs(i, j+1) + 1)
+            # try replacing 
+            res = min(res, dfs(i+1, j+1) + 1)
+
+            memo[(i, j)] = res
+            return memo[(i, j)]
+            
+        return dfs(0, 0)
