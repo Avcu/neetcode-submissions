bd98fc45c4ef7e117e42740e2f9ba399b484ebbc
Mustafa Avcu <itsavcu@gmail.com>
2026-09-06 22:57:10 -0400

    Add: triangle - submission-0

---
+++ Data Structures & Algorithms/triangle/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+
+        for i in range(1, n):
+            for j in range(i+1):
+                if j == 0:
+                    triangle[i][j] += triangle[i-1][j]
+                elif j == i:
+                    triangle[i][j] += triangle[i-1][j-1]
+                else:
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j])
+        
+        return min(triangle[-1][:])
